40c72e6ba0dbe372a031485ac41a5b3105f40eab
Kunal Gangani <kunalgangani009@gmail.com>
2026-08-02 02:41:13 +0530

    Initial Commit

---
+++ task-manager-auth/task-manager-auth/src/main/java/com/example/task_manager_auth/TaskManagerAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.task_manager_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerAuthApplication.class, args);
+	}
+
+}
---
+++ task-manager-auth/task-manager-auth/src/test/java/com/example/task_manager_auth/TaskManagerAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_manager_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
